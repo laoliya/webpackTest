a0e3576f6b8dbcd7a31d0d6b0181fcaf4934f582
Luffy <936366286@qq.com>
2023-10-29 22:24:28 +0800

    fix: style-loader css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
-const path = require('path');
+const path = require("path");
+
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename:'bundle.js'
-    }
-}
+  mode: "development",
+  devtool: "source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
